d7b0f81ce7c4c0cc0a9355ea3e0f2e59422f26c9
张一鹏 <yipeng.zhang@zymobi.com>
2020-06-12 15:03:51 +0800

    5：(适配4.5引擎)移除AFNetworking.framework，依赖引擎公共的AF

--- EUExUploaderMgr/EUExUploaderMgr/sourceCode/AFNetworking.framework/Headers/AFNetworking.h
+++ EUExUploaderMgr/EUExUploaderMgr/sourceCode/AFNetworking.framework/Headers/AFNetworking.h
@@ -51,7 +51,6 @@
 #if TARGET_OS_IOS
 #import <AFNetworking/AFNetworkActivityIndicatorManager.h>
 #import <AFNetworking/UIRefreshControl+AFNetworking.h>
-#import <AFNetworking/UIWebView+AFNetworking.h>
 #endif
 
 
